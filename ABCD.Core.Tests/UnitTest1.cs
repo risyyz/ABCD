af47dfa8862a51df4f6b497bfe38adc00cdc5a46
risyyz <rishi.tapsee@gmail.com>
2024-11-06 09:50:03 -0500

    #1 - unit tests init

---
+++ ABCD.Core.Tests/UnitTest1.cs
@@ -0,0 +1,8 @@
+namespace ABCD.Core.Tests {
+    public class UnitTest1 {
+        [Fact]
+        public void Test1() {
+
+        }
+    }
+}
---
+++ ABCD.Server.Tests/UnitTest1.cs
@@ -0,0 +1,8 @@
+namespace ABCD.Server.Tests {
+    public class UnitTest1 {
+        [Fact]
+        public void Test1() {
+
+        }
+    }
+}
