43351b58bba127e321f8c42aa124f8330ff82c02
risyyz <rishi.tapsee@gmail.com>
2025-02-26 22:23:33 -0500

    #5 #6 Token class to represent JWT and RefreshToken

---
+++ ABCD.Lib/Token.cs
@@ -0,0 +1,7 @@
+﻿namespace ABCD.Lib {
+    public record Token {
+        public required string JWT { get; init; }
+        public required string RefreshToken { get; init; }
+        public bool IsEmpty { get => string.IsNullOrWhiteSpace(JWT) && string.IsNullOrWhiteSpace(RefreshToken); }
+    }
+}
