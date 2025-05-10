e77e30be17123bb75639982550d06b15f61b02d0
risyyz <rishi.tapsee@gmail.com>
2025-05-10 15:33:33 -0400

    #9 style the login screen

--- abcd.client/src/app/auth/auth.module.ts
+++ abcd.client/src/app/auth/auth.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
+  declarations: [LoginComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule
   ]
 })
 export class AuthModule { }
